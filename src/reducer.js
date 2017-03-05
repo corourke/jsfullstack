463fd770243c95aecf1d5bb57850f0d35cf76755
Cameron O'Rourke <cameron.orourke@oracle.com>
2017-03-04 21:53:44 -0800

    store configured

---
+++ src/reducer.js
@@ -0,0 +1,18 @@
+/**
+ * Created by cameron on 2/19/17.
+ */
+
+import {INITIAL_STATE, setEntries, next, vote} from './core';
+
+export default function reducer(state = INITIAL_STATE, action) {
+    switch (action.type) {
+        case 'SET_ENTRIES':
+            return setEntries(state, action.entries);
+        case 'NEXT':
+            return next(state);
+        case 'VOTE':
+            return state.update('vote',
+                voteState => vote(voteState, action.entry));
+    }
+    return state;
+}
